4ad73ac7f7f17bd50d6d5fb12396932e2a301801
krausyd <krausyd@hotmail.com>
2022-01-10 17:00:07 -0800

    (∩◕-◕ )⊃━✿✿✿✿  Styling

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const { writeFile, copyFile } = require('./util/generate-site');
 const generatePage = require('./src/page-template');
 const inquirer = require('inquirer');
 const promptUser = () => {
@@ -130,10 +130,15 @@ Add a New Project
 
 promptUser()
     .then(promptProject)
-    .then(portfolioData => {    
-        const pageHTML = generatePage(portfolioData);
-        fs.writeFile('./index.html', pageHTML, err => {
-            if (err) throw new Error(err);
-            console.log('Page created! Check out index.html in this directory to see it!');
-        });
+    .then(portfolioData => generatePage(portfolioData))
+    .then(pageHTML => writeFile(pageHTML))
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile()
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err)
     });
